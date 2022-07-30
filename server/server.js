b5d4ad9095970568e1f59705d1cc30705b01cf98
EmpAsh938 <ashokthapa938@gmail.com>
2022-07-30 08:31:55 +0545

    typescript removed for normal setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv');
 
+// initialzier
 const app = express();
+dotenv.config();
+
+// defaults and middleware
 const port = process.env.PORT || 8100;
 
+// routes
 app.get('/', (req,res) => {
     res.json({
         message:"Home",
     })
 })
 
+// listener
 app.listen(port, () => {
     console.log("Server running on "+port);
 })
